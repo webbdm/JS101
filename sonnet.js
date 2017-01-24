3fb6dc21dfc4a259a41f496eb1f3c526c59b922d
webbdm <gwebb762@gmail.com>
2017-01-24 17:58:59 -0600

    started sonnet exercise

---
+++ sonnet.js
@@ -0,0 +1,14 @@
+var element = document.getElementById("sonnet");
+
+var sonnet = element.innerHTML;
+
+console.log(sonnet);
+
+
+orphanPosition = sonnet.indexOf("orphans");
+
+console.log(orphanPosition);
+
+console.log(sonnet.length);
+
+var newLine = sonnet.replace
