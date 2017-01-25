8e309ef1b02dbb57bc33b829ba371470ac5fdb58
webbdm <gwebb762@gmail.com>
2017-01-24 19:19:43 -0600

    added switch

---
+++ day2.js
@@ -0,0 +1,13 @@
+//switch statement
+
+var day = "Friday";
+
+switch(day){
+	case "Monday":
+		console.log("Boooooooo");
+	case  "Friday":
+		console.log("PARTAY");
+		break;
+	default:
+		console.log("not Monday");
+}
